89c653320f0364811cf9aaa272ae3e9bc31a05d6
nejdetkadir <nejdetkadir.550@gmail.com>
2021-09-03 15:50:15 +0300

    Add category as references to product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_03_104350) do
+ActiveRecord::Schema.define(version: 2021_09_03_124858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2021_09_03_104350) do
     t.integer "view_count", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
@@ -67,5 +69,6 @@ ActiveRecord::Schema.define(version: 2021_09_03_104350) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "categories"
   add_foreign_key "products", "users"
 end
