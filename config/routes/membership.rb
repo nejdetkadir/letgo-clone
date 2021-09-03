eda0c0003390538c75c5bac7387c66afe36ded56
nejdetkadir <nejdetkadir.550@gmail.com>
2021-09-04 00:13:18 +0300

    Design index page of home controller of membership module

--- config/routes/membership.rb
+++ config/routes/membership.rb
@@ -1,6 +1,10 @@
 authenticate :user do
   scope 'uyelik', module: 'membership', as: 'membership' do
-    # get '/', to: 'home#index', as: 'root'
+    get '/', to: 'home#index', as: 'root'
+    
+    # scope 'ayarlar', as: 'settings' do
+    #   get '/', to: 'profile#index', as: 'root'
+    # end
 
     scope 'ilan', as: 'product' do
       post '/yeni', to: 'products#create', as: 'create'
