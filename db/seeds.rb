565a993b846caa2cc8c2712caa397e8e18a55bc0
nejdetkadir <nejdetkadir.550@gmail.com>
2021-09-03 18:13:25 +0300

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Categories
+
+User.create(email: "nejdetkadir.550@gmail.com", password: "123456789")
+
 Category.create(name: "Elektronik", icon: "fas-mobile", color: "#6bcebb")
 Category.create(name: "Spor, Eğlence ve Oyunlar", icon: "fas-basketball-ball", color: "#a3ce71")
 Category.create(name: "Araba", icon: "fas-car", color: "#327fb6")
@@ -15,3 +19,20 @@ Category.create(name: "Moda ve Aksesuar", icon: "fas-shoe-prints", color: "#fd7d
 Category.create(name: "Bebek ve Çocuk", icon: "fas-baby-carriage", color: "#20c3f3")
 Category.create(name: "Film, Kitap ve Müzik", icon: "fas-headphones", color: "#ec7ebd")
 Category.create(name: "Diğer", icon: "fab-buffer", color: "#e1c340")
+
+10.times {
+  User.last.products.create(
+    name: Faker::Book.title,
+    category: Category.active.sample,
+    description: Faker::Lorem.sentence,
+    price: "1500.00",
+    remote_images_urls: [
+      Faker::Avatar.image(slug: Faker::Book.title, size: "800x800", format: "jpg"),
+      Faker::Avatar.image(slug: Faker::Book.title, size: "800x800", format: "jpg"),
+      Faker::Avatar.image(slug: Faker::Book.title, size: "800x800", format: "jpg"),
+      Faker::Avatar.image(slug: Faker::Book.title, size: "800x800", format: "jpg"),
+      Faker::Avatar.image(slug: Faker::Book.title, size: "800x800", format: "jpg"),
+      Faker::Avatar.image(slug: Faker::Book.title, size: "800x800", format: "jpg"),
+    ]
+  )
+}
