b83042f3309c54d90668a4b0a6a2f1389c7771ed
nejdetkadir <nejdetkadir.550@gmail.com>
2021-09-05 13:25:40 +0300

    Paginate current user products

--- app/controllers/membership/home_controller.rb
+++ app/controllers/membership/home_controller.rb
@@ -1,4 +1,7 @@
 class Membership::HomeController < MembershipController
   def index
+    @sold = current_user.products.sold.page(params[:page])
+    @selling = current_user.products.selling.page(params[:page])
+    @favorites = current_user.favorites.page(params[:page])
   end
 end
