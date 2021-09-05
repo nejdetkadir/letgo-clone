771dabda0595370e668e359ef64ed009f5e36afd
nejdetkadir <nejdetkadir.550@gmail.com>
2021-09-05 15:34:52 +0300

    Show products on show page of categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.active.friendly.find(params[:slug])
+    @products = user_signed_in? ? @category.products.selling.where.not(user: current_user).page(params[:page]) : @category.products.selling.page(params[:page])
   end
 end
