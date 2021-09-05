0440c8ef854d78e0c32da076d3145864eeb08492
nejdetkadir <nejdetkadir.550@gmail.com>
2021-09-05 13:51:28 +0300

    Create and design footer

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -8,6 +8,8 @@ class Setting < RailsSettings::Base
   # field :default_locale, default: "en", type: :string
   # field :confirmable_enable, default: "0", type: :boolean
   # field :admin_emails, default: "admin@rubyonrails.org", type: :array
+  field :app_store_url, type: :string, default: "https://apps.apple.com/app/letgo-buy-sell-used-stuff/id986339882"
+  field :google_play_url, type: :string, default: "https://play.google.com/store/apps/details?id=com.abtnprojects.ambatana"
   
   # Omniauths
   field :omniauth_google_client_id, default: "", type: :string
